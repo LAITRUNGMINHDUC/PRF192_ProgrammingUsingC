c1ee7b8d2b13a32dadad17164b6ec82f323666bb
Duc Lai Trung Minh (Henry Lai) <laitrungminhduc@hotmail.com>
2016-03-14 22:24:19 +0700

    Thêm Return 0 vào trong hàm main
    Sửa file AddressingVariable.c

---
+++ Introduction/LoRon2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Yeu em biet de vao dau\n");
+	printf("De vao lo ron, lau lau lai ro\n");
+	getchar(); // The wait-command for press any key
+	return 0;		
+}
